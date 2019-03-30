333c37b8cde68d78eb224e264aa300dadaacdcfe
Santosh Marigowda <santosharakere@gmail.com>
2019-03-30 09:11:06 +0000

    NEXT-1 links without using client side history api

---
+++ portfolio/pages/about.js
@@ -0,0 +1,10 @@
+export default () => {
+    return (
+        <div>
+            <h1>About Page</h1>
+            <a href="/">Home Link</a>
+            <p>JS Programmer</p>
+            <img src="static/js-logo.png" alt="Java Script Logo"></img>
+        </div>
+    )
+}
--- portfolio/pages/index.js
+++ portfolio/pages/index.js
@@ -1,6 +1,8 @@
 const Index = () => {
     return (<div>
-        <h1>Welcome to my Home Page</h1>
+        <h1>Home</h1>
+        <a href="/about">About Link</a>
+        <p>Welcome to my Home Page</p>
     </div>);
 }
 
