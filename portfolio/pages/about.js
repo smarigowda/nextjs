a7fac263c5b54d3bd791ac607553ca3cc83dc7e2
Santosh Marigowda <santosharakere@gmail.com>
2019-03-31 08:41:16 +0100

    NEXT-1 using componentDidMount to fetch data @ client side

--- portfolio/pages/about.js
+++ portfolio/pages/about.js
@@ -1,14 +1,40 @@
 import Link from 'next/link';
 import Layout from '../components/Layout';
-const About = () => {
-    return (
-        <div>
-            <Layout title="About Page">
-                <p>JS Programmer</p>
-                <img src="static/js-logo.png" alt="Java Script Logo"></img>
-            </Layout>
-        </div>
-    )
-};
+import { Component } from 'react';
 
-export default About;
+export default class About extends Component {
+    state = {
+        user: null,
+    }
+    componentDidMount() {
+        console.log('-- componentDidMount --');
+        fetch('https://api.github.com/users/smarigowda')
+        .then(res => res.json())
+        .then(data => {
+            this.setState( { user: data });
+        });
+    }
+    render() {
+        return (
+            <div>
+                <Layout title="About Page">
+                    <p>JS Programmer</p>
+                    {JSON.stringify(this.state.user)}
+                    <img src="static/js-logo.png" alt="Java Script Logo"></img>
+                </Layout>
+            </div >
+        )
+    }
+}
+// const About = () => {
+//     return (
+//         <div>
+//             <Layout title="About Page">
+//                 <p>JS Programmer</p>
+//                 <img src="static/js-logo.png" alt="Java Script Logo"></img>
+//             </Layout>
+//         </div>
+//     )
+// };
+
+// export default About;
