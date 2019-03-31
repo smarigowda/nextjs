e5d791510a1647f243978530a5e81a635a700d9e
Santosh Marigowda <santosharakere@gmail.com>
2019-03-31 09:08:38 +0100

    NEXT-1 fetching data on server side

--- portfolio/pages/about.js
+++ portfolio/pages/about.js
@@ -1,27 +1,38 @@
 import Link from 'next/link';
 import Layout from '../components/Layout';
 import { Component } from 'react';
+import fetch from 'isomorphic-unfetch';
 
 export default class About extends Component {
-    state = {
-        user: null,
-    }
-    componentDidMount() {
-        console.log('-- componentDidMount --');
-        fetch('https://api.github.com/users/smarigowda')
-        .then(res => res.json())
-        .then(data => {
-            this.setState( { user: data });
-        });
+    // state = {
+    //     user: null,
+    // }
+    // componentDidMount() {
+    //     console.log('-- componentDidMount --');
+    //     fetch('https://api.github.com/users/smarigowda')
+    //     .then(res => res.json())
+    //     .then(data => {
+    //         this.setState( { user: data });
+    //     });
+    // }
+    static async getInitialProps() {
+        const result = await fetch('https://api.github.com/users/smarigowda')
+        const data = await result.json();
+        
+        //     .then(res => res.json())
+        //     .then(data => {
+        //         return data;
+        //     });
+        return { user: data }
     }
     render() {
         return (
             <div>
                 <Layout title="About Page">
                     <p>JS Programmer</p>
-                    {/* {JSON.stringify(this.state.user)} */}
-                    {/* <img src="static/js-logo.png" alt="Java Script Logo"></img> */}
-                    <img src={this.state.user ? this.state.user.avatar_url : ''} alt="Java Script Logo"></img>
+                    {JSON.stringify(this.props.user)}
+                    <img src="static/js-logo.png" alt="Java Script Logo"></img>
+                    {/* <img src={this.state.user ? this.state.user.avatar_url : ''} alt="Java Script Logo"></img> */}
                 </Layout>
             </div >
         )
